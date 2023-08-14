05ee4c59714ae30d48c1a5625bbf70e26d1d507c
IvanMakarishchev <denis.flank@gmail.com>
2023-08-14 15:01:53 +0200

    feat: add migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PostModule } from './post/post.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigService } from '@nestjs/config';
-import { User } from './post/user/entities/user.entity';
-import { Track } from './post/track/entities/track.entity';
-import { Artist } from './post/artist/entities/artist.entity';
-import { Album } from './post/album/entities/album.entity';
-import { FavArtists } from './post/favorites/artist/entities/artist.entity';
-import { FavTracks } from './post/favorites/track/entities/track.entity';
-import { FavAlbums } from './post/favorites/album/entities/album.entity';
+import { dataSourseOptions } from 'db/data-source';
 
 @Module({
   imports: [
@@ -17,25 +10,8 @@ import { FavAlbums } from './post/favorites/album/entities/album.entity';
     PostModule,
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        synchronize: true,
-        entities: [
-          User,
-          Track,
-          Artist,
-          Album,
-          FavArtists,
-          FavTracks,
-          FavAlbums,
-        ],
-      }),
       inject: [ConfigService],
+      useFactory: () => dataSourseOptions(),
     }),
   ],
   controllers: [],
